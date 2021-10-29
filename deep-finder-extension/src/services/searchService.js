6230284bda288bbc41dbeeb54082f79ebbc6f4df
robsli <9888125+robsli@users.noreply.github.com>
2021-10-29 17:12:55 -0400

    Update searchService to handle for prod and local development endpoints

--- deep-finder-extension/src/services/searchService.js
+++ deep-finder-extension/src/services/searchService.js
@@ -1,9 +1,33 @@
 const API_ROOT = 'https://placeholder.io/something'
+const MOCK_API_ROOT = 'http://localhost:3004';
 
 const search = async (query, text) => {
 
-	const response = await fetch();
-	const resp = response.json();
+	if (process.env.NODE_ENV !== 'production') {
+		return getMockData();
+	};
+
+	const response = await fetch(`${API_ROOT}/searchResults`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ query, text })
+	});
+
+	return response.json();
+}
+
+const getMockData = async () => {
+
+	const response = await fetch(`${MOCK_API_ROOT}/searchResults`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	return response.json();
 }
 
 export { search };
