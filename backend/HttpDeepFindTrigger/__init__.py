c3d295021b5941d9debfb9b2a5a270396520e151
Chris Kabat <chriskabat@outlook.com>
2021-10-29 23:31:15 -0500

    Added more realistic parameters

--- backend/HttpDeepFindTrigger/__init__.py
+++ backend/HttpDeepFindTrigger/__init__.py
@@ -1,24 +1,39 @@
 import logging
-
 import azure.functions as func
+import json
+import metapy
+import pytoml
 
+import sharedcode.ranker as ranker
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    query = req.params.get('query')
+    if not query:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            query = req_body.get('query')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    passages = req.params.get('passages')
+    if not passages:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            passages = req_body.get('passages')
+
+    rk = ranker.PassageRanker(query,passages)
+
+    if query and len(passages)>0:
+        returnstring = rk.process()
+        return func.HttpResponse(returnstring)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully, but invalid parameters were passed. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
