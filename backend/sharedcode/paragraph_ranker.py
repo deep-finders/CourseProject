5e8c6b90c44ccd8b19faf2491de6ffa9ec80b6c8
Chris Kabat <53019845+chriskabat63@users.noreply.github.com>
2021-11-03 15:10:07 -0500

    Backend (#6)
    * Added more realistic parameters
    
    * Updated with BM25 algorithm
    
    * Made changes to input structure and temporarily mocked up results

--- backend/sharedcode/paragraph_ranker.py
+++ backend/sharedcode/paragraph_ranker.py
@@ -7,98 +7,172 @@ import numpy as np
 from goose3 import Goose
 import sys
 import json
-import argparse
+import nltk
+import uuid
+
+class ParagraphRanker:
+    def __init__(self):
+        nltk.download('punkt')
+    
+    # Clean stopwords
+    def remove_stopwords(self,paragraphs):
+        sw_removed_paragraphs = list()
+        for paragraph in paragraphs:
+            paragraph_tokens = word_tokenize(paragraph)
+            tokens_without_sw = [word for word in paragraph_tokens if word not in STOPWORDS]
+            sw_removed_paragraph = ' '.join(tokens_without_sw)
+            sw_removed_paragraphs.append(sw_removed_paragraph)
+
+        return sw_removed_paragraphs
 
 
-# Clean stopwords
-def remove_stopwords(paragraphs):
-    sw_removed_paragraphs = list()
-    for paragraph in paragraphs:
-        paragraph_tokens = word_tokenize(paragraph)
-        tokens_without_sw = [word for word in paragraph_tokens if word not in STOPWORDS]
-        sw_removed_paragraph = ' '.join(tokens_without_sw)
-        sw_removed_paragraphs.append(sw_removed_paragraph)
+    # Stem paragraphs
+    def stem_paragraphs(self,paragraphs):
+        ps = PorterStemmer()
+        stemmed_paragraphs = list()
+        for paragraph in paragraphs:
+            paragraph_tokens = word_tokenize(paragraph)
+            stemmed_tokens = [ps.stem(word) for word in paragraph_tokens]
+            stemmed_paragraph = ' '.join(stemmed_tokens)
+            stemmed_paragraphs.append(stemmed_paragraph)
 
-    return sw_removed_paragraphs
-
-
-# Stem paragraphs
-def stem_paragraphs(paragraphs):
-    ps = PorterStemmer()
-    stemmed_paragraphs = list()
-    for paragraph in paragraphs:
-        paragraph_tokens = word_tokenize(paragraph)
-        stemmed_tokens = [ps.stem(word) for word in paragraph_tokens]
-        stemmed_paragraph = ' '.join(stemmed_tokens)
-        stemmed_paragraphs.append(stemmed_paragraph)
-
-    return stemmed_paragraphs
+        return stemmed_paragraphs
 
 
-# Remove empty paragraphs and new lines only paragraphs
-def remove_empty_paragraphs(paragraphs):
-    cleaned_paragraphs = list()
-    for paragraph in paragraphs:
-        if paragraph == '' or paragraph == '\n':
-            continue
-        cleaned_paragraphs.append(paragraph)
+    # Remove empty paragraphs and new lines only paragraphs
+    def remove_empty_paragraphs(self,paragraphs):
+        cleaned_paragraphs = list()
+        for paragraph in paragraphs:
+            if paragraph == '' or paragraph == '\n':
+                continue
+            cleaned_paragraphs.append(paragraph)
 
-    return np.array(cleaned_paragraphs)
-
-
-# Build paragraphs list based on raw html and mode
-def get_paragraphs(raw_html, mode, split_by, num_elements):
-    paragraphs = []
-
-    # Builds paragraphs from split article based on multiples of num_elements
-    if mode == 'pseudo':
-
-        # Retrieve the entire cleaned article
-        g = Goose()
-        article = g.extract(raw_html=raw_html)
-        lines = article.cleaned_text.split(split_by)
-
-        # Form paragraphs from multiples of num_elements
-        i = 0
-        while i < len(lines):
-            paragraph = ''
-            sentences = lines[i:i + num_elements]
-            for sentence in sentences:
-                paragraph += sentence
-            paragraphs.append(paragraph)
-            i += num_elements
-
-        return np.array(paragraphs)
-
-    # Builds paragraphs from <p> and <td> tags found in the html
-    elif mode == 'tag':
-
-        soup = BeautifulSoup(raw_html, 'html.parser')
-
-        # Get all <p> tags
-        p_tags = soup.findAll('p')
-
-        # Get all <td> tags
-        td_tags = soup.findAll('td')
-
-        # Get inner text of <p> and <td> tags to get list of paragraphs
-        p_paragraphs = [tag.text for tag in p_tags]
-        td_paragraphs = [tag.text for tag in td_tags]
-
-        # Only include <td> paragraphs if available, only <p> paragraphs otherwise
-        if len(td_paragraphs) > 0:
-            paragraphs = p_paragraphs + td_paragraphs
-        else:
-            paragraphs = p_paragraphs
-
-        return np.array(paragraphs)
+        return np.array(cleaned_paragraphs)
 
 
-if __name__ == "__main__":
+    # Build paragraphs list based on raw html and mode
+    def get_paragraphs(self,raw_html, mode, split_by, num_elements):
+        paragraphs = []
 
-    # Read command line arguments
-    parser = argparse.ArgumentParser()
+        # Builds paragraphs from split article based on multiples of num_elements
+        if mode == 'pseudo':
 
+            # Retrieve the entire cleaned article
+            g = Goose()
+            article = g.extract(raw_html=raw_html)
+            lines = article.cleaned_text.split(split_by)
+
+            # Form paragraphs from multiples of num_elements
+            i = 0
+            while i < len(lines):
+                paragraph = ''
+                sentences = lines[i:i + num_elements]
+                for sentence in sentences:
+                    paragraph += sentence
+                paragraphs.append(paragraph)
+                i += num_elements
+
+            return np.array(paragraphs)
+
+        # Builds paragraphs from <p> and <td> tags found in the html
+        elif mode == 'tag':
+            soup = BeautifulSoup(raw_html, 'html.parser')
+
+            # Get all <p> tags
+            p_tags = soup.findAll('p')
+
+            # Get all <td> tags
+            td_tags = soup.findAll('td')
+
+            # Get inner text of <p> and <td> tags to get list of paragraphs
+            p_paragraphs = [tag.text for tag in p_tags]
+            td_paragraphs = [tag.text for tag in td_tags]
+
+            # Only include <td> paragraphs if available, only <p> paragraphs otherwise
+            if len(td_paragraphs) > 0:
+                paragraphs = p_paragraphs + td_paragraphs
+            else:
+                paragraphs = p_paragraphs
+
+            return np.array(paragraphs)
+    
+    def search(self,raw_html,query,top_n,mode,split_by,num_elements):
+
+        # Build list of paragraphs
+        paragraphs = self.get_paragraphs(raw_html, mode, split_by, num_elements)
+        # Remove any empty paragraphs
+        paragraphs = self.remove_empty_paragraphs(paragraphs)
+
+        #clean up a couple parameters
+        top_n = int(top_n)
+        num_elements = int(num_elements)
+
+        '''
+        Observation: num_elements in pseudo mode should be adjusted such that 
+        len(paragraphs) > n where n is the number of relevant paragraphs returned 
+        by the ranker.
+        '''
+        print('Number of paragraphs: {}'.format(len(paragraphs)))
+
+        # Remove stop words from paragraphs
+        paragraphs_clean = self.remove_stopwords(paragraphs)
+
+        # Optional stem paragraphs, doesn't seem to always be an improvement
+        # paragraphs_clean = stem_paragraphs(paragraphs_clean)
+
+
+        # Tokenize the corpus for BM25 model
+        tokenized_corpus = [doc.split(' ') for doc in paragraphs_clean]
+
+        # Initialize BM25 model, currently using default parameters k1=1.5, b=0.75
+        bm25 = BM25Okapi(tokenized_corpus)
+
+        print("Query: {}".format(query))
+        print("Num_Elements: {}".format(num_elements))
+        print("Top N: {}".format(top_n))
+        print("Mode: {}".format(mode))
+        print("Split_By: {}".format(split_by))
+
+        # Tokenize query for BM25 retrieval
+        tokenized_query = query.split()
+
+        # Get scores for all paragraphs
+        doc_scores = bm25.get_scores(tokenized_query)
+        print('BM25 Scores:')
+        print(doc_scores)
+
+        # adjust top docs based on number of docs
+        if len(tokenized_corpus)<top_n:
+            top_n=len(tokenized_corpus)
+
+        # Get the top n scores
+        top_n_scores_idx = [score for score in reversed(np.argsort(doc_scores)[-top_n:])]
+
+        # Get the top n paragraphs in their original form
+        top_n_docs = paragraphs[top_n_scores_idx]
+
+        results = list()
+
+        # Print rank and paragraph
+        print('Paragraph Rankings:')
+        for idx, doc in enumerate(top_n_docs):
+            doc_dict = {}
+            rank = idx + 1
+            doc_dict['id'] = str(uuid.uuid4())
+            doc_dict['rank'] = rank
+            doc_dict['score'] = doc_scores[top_n_scores_idx[idx]]
+            doc_dict['passage'] = doc
+            if  doc_dict['score']>0:
+                results.append(doc_dict)
+            print('Rank {}: '.format(rank) + doc)
+
+        json_return = json.dumps(results)
+        print(json_return)
+        return json_return
+
+
+def main(argv):
+    pr=ParagraphRanker()
     parser.add_argument('-r', '--raw_html', type=str, required=True,
                         help='The raw html to return paragraph rankings for')
     parser.add_argument('-q', '--query', type=str, required=True, help='The query to be searched')
@@ -143,67 +217,9 @@ if __name__ == "__main__":
     if args['b']:
         b = args['b']
 
-    # Build list of paragraphs
-    paragraphs = get_paragraphs(raw_html, mode=mode, split_by=split_by, num_elements=num_elements)
-    # Remove any empty paragraphs
-    paragraphs = remove_empty_paragraphs(paragraphs)
+    return pr.search(raw_html,query,top_n,mode,split_by,num_elements)
+ 
 
-    '''
-    Observation: num_elements in pseudo mode should be adjusted such that 
-    len(paragraphs) > n where n is the number of relevant paragraphs returned 
-    by the ranker.
-    '''
-    print('Number of paragraphs: {}'.format(len(paragraphs)))
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
-    # Remove stop words from paragraphs
-    paragraphs_clean = remove_stopwords(paragraphs)
-
-    # Optional stem paragraphs, doesn't seem to always be an improvement
-    # paragraphs_clean = stem_paragraphs(paragraphs_clean)
-
-    # Tokenize the corpus for BM25 model
-    tokenized_corpus = [doc.split(' ') for doc in paragraphs_clean]
-
-    # Initialize BM25 model, currently using default parameters k1=1.5, b=0.75
-    bm25 = BM25Okapi(tokenized_corpus, k1=k1, b=b)
-
-    print("Query: {}".format(query))
-    print("Num_Elements: {}".format(num_elements))
-    print("Top N: {}".format(top_n))
-    print("Mode: {}".format(mode))
-    print("Split_By: {}".format(split_by))
-    print("k1: {}".format(k1))
-    print("b: {}".format(b))
-
-    # Tokenize query for BM25 retrieval
-    tokenized_query = query.split()
-
-    # Get scores for all paragraphs
-    doc_scores = bm25.get_scores(tokenized_query)
-    print('BM25 Scores:')
-    print(doc_scores)
-
-    # Get the top n scores
-    top_n_scores_idx = [score for score in reversed(np.argsort(doc_scores)[-top_n:])]
-
-    # Get the top n paragraphs in their original form
-    top_n_docs = paragraphs[top_n_scores_idx]
-
-    results = []
-
-    # Print rank and paragraph
-    print('Paragraph Rankings:')
-    for idx, doc in enumerate(top_n_docs):
-        doc_dict = {}
-        rank = idx + 1
-        doc_dict['rank'] = rank
-        doc_dict['score'] = doc_scores[top_n_scores_idx[idx]]
-        doc_dict['passage'] = doc
-        results.append(doc_dict)
-        print('Rank {}: '.format(rank) + doc)
-
-    # Create json result and write to output file
-    json_return = json.dumps(results)
-
-    with open('output/ranking_result.json', 'w') as file:
-        json.dump(json_return, file)
